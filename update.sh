b108d604665de094931ffde0726678efb89fdaa8
HeoYou <lov21y_@naver.com>
2022-04-28 23:55:12 +0900

    jekyll upload

---
+++ update.sh
@@ -0,0 +1,10 @@
+git checkout -b source
+git branch -D main
+git add .
+git commit -m "jekyll upload"
+git checkout -b main
+git filter-branch --subdirectory-filter _site -f HEAD
+git add .
+git commit -m "jekyll"
+git push --all
+git push -u origin main --force

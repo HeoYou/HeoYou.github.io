1c884710803d067473fa9b8709a4cbfc90f6b3b4
HeoYou <lov21y_@naver.com>
2022-05-19 23:32:43 +0900

    cURL

--- _site/update.sh
+++ _site/update.sh
@@ -1,11 +1,11 @@
 git checkout -b source
 git branch -D main
 git add .
-git commit -m "jekyll upload"
+git commit -m "cURL"
 git checkout -b main
 git filter-branch --subdirectory-filter _site -f HEAD
 git add .
-git commit -m "jekyll"
+git commit -m "cURL"
 git push --all
 git push -u origin main --force
 git checkout source
